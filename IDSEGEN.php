6e9d2fdae85794a095c3e225828ba4c7f3d19e64
GabrielCP93 <gabriel.castrop93@gmail.com>
2021-02-02 15:44:06 -0600

    Se Agrego Relacion de movimientos operados IDSEGEN

--- IDSEGEN.php
+++ IDSEGEN.php
@@ -1,4 +1,4 @@
-<?php
+<?
     /*Lista de Variables:
         variable1:Razón Social
         variable2:Registro Patronal
@@ -33,7 +33,7 @@
     function formato($status,$variable1,$variable2,$variable3,$variable4,$variable5,$variable6,$variable7,$variable8,$variable9,
     $variable10,$variable11,$variable12,$variable13,$variable14,$variable15,$variable16,$variable17,$variable18,$variable19,$variable20){
         mysql_connect("localhost","root","");
-        require_once('C:\xampp\htdocs\mio\pdf\mpdf.php');
+        require_once('/opt/lampp/htdocs/mpdf/app/reports/mio/pdf/mpdf.php');
         $mpdf = new mPDF('c','A4');
         $html = ('<html>
             <head>
@@ -63,31 +63,22 @@
                     </tr>
                     <tr>
                         <td class="p_style5"><span class="style4">Número de folio: '.$variable3.'</span></td>
-                        <!-- <td class="p_style6"><span class="style2">8700907963241087340</span></td>-->
                         <td class="m_style1"> </td>
                         <td class="p_style6" rowspan="2"><span class="style4">Razón Social: '.$variable1.'</span></td>
-                        <!--<td class="p_style7" rowspan="2"><span class="style2">#VARIABLE</span></td>-->
                     </tr>
                     <tr>
                         <td class="p_style5"><span class="style4">Número de lote: '.$variable4.'</span></td>
-                        <!--<td class="p_style6"><span class="style4">#VARIABLE</span></td>-->
                         <td class="m_style1"> </td>
-                        <!--<td style="border: none;"> </td>
-                            <td style="border: none;"> </td>-->
                     </tr>
                     <tr>
                         <td class="p_style5"><span class="style4">RFC del patrón: '.$variable5.'</span></td>
-                        <!--<td class="p_style6"><span class="style2">#Variable</span></td>-->
                         <td class="m_style1"> </td>
                         <td class="p_style5" style="width: 65;"><span class="style4">Fecha y hora de recepción del lote: '.$variable6.'</span></td>
-                        <!--<td class="p_style8"><span class="style2">#Variable</span></td>-->
                     </tr>
                     <tr>
                         <td class="p_style5"><span class="style4">Registro Patronal: '.$variable2.'</span></td>
-                        <!--<td class="p_style6"><span class="style2">#Variable</span></td>-->
                         <td class="m_style1"> </td>
                         <td class="p_style5"><span class="style4">Serial del certificado: '.$variable7.'</span></td>
-                        <!--<td class="p_style9"><span class="style2">#Variable</span></td>-->
                     </tr>
                 </table>
                 <table style="width: 100%; margin-top: 10; margin-bottom: 0;">
@@ -102,26 +93,26 @@
                         </td>
                     </tr>
                 </table>
-                <table style="width: 100%; margin-top: 10; margin-bottom: 0;" cellpadding="5" >
-                    <tr style="width: 100%;">
-                        <td style="width: 224px;">
+                <table style="width: 100%; margin-top: 10; margin-bottom: 5;" cellpadding="5" cellspacing= "3">
+                    <tr>
+                        <td style="width: 280px;">
                             <table style="width: 100%;">
-                                <tr>
-                                    <td class="t_style3" colspan="4">
+                                <tr style="width: 100%;">
+                                    <td style="width: 100%;"class="t_style3" colspan="4">
                                         <span class="style3">Movimientos recibidos por el IMSS</span>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:75px;" class="t_style4">
                                         <span class="style3">Bajas</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:65px;" class="t_style4">
                                         <span class="style3">Modif.</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:65px;" class="t_style4">
                                         <span class="style3">Reing.</span>
                                     </td>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:75px;" class="t_style4">
                                         <span class="style3">Total</span>
                                     </td>
                                     <tr>
@@ -141,24 +132,24 @@
                                 </tr>
                             </table>
                         </td>
-                        <td style="width: 224px;">
+                        <td style="width: 250px;">
                             <table style="width: 100%;">
-                                <tr>
-                                    <td class="t_style3" colspan="4">
+                                <tr style="width: 100%;">
+                                    <td style="width: 100%;" class="t_style3" colspan="4">
                                         <span class="style3">Operados</span>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:65px;" class="t_style4">
                                         <span class="style3">Bajas</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:60px;" class="t_style4">
                                         <span class="style3">Modif.</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:60px;" class="t_style4">
                                         <span class="style3">Reing.</span>
                                     </td>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:65px;" class="t_style4">
                                         <span class="style3">Total</span>
                                     </td>
                                 </tr>
@@ -178,24 +169,24 @@
                                 </tr>
                             </table>
                         </td>
-                        <td style="width: 224px">
+                        <td style="width: 260px">
                             <table style="width: 100%;">
-                                <tr>
-                                    <td class="t_style3" colspan="4">
+                                <tr style="width: 100%;">
+                                    <td style="width: 100%;" class="t_style3" colspan="4">
                                         <span class="style3">Rechazados</span>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:70px;" class="t_style4">
                                         <span class="style3">Bajas</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:60px;" class="t_style4">
                                         <span class="style3">Modif.</span>
                                     </td>
-                                    <td style="width:55px;" class="t_style4">
+                                    <td style="width:60px;" class="t_style4">
                                         <span class="style3">Reing.</span>
                                     </td>
-                                    <td style="width:57px;" class="t_style4">
+                                    <td style="width:70px;" class="t_style4">
                                         <span class="style3">Total</span>
                                     </td>
                                 </tr>
@@ -218,31 +209,42 @@
                     </tr>
                 </table>
                 <table>
-                    <tr><td colspan="10" style="text-align: center; width: 100%;"><span>Relación de movimientos operados</span></td></tr>
-                    <tr>
-                        <td><span>Tipo</span></td>
-                        <td><span>NSS</span></td>
-                        <td><span>Nombre Asegurado</span></td>
-                        <td><span>Sal.Base</span></td>
-                        <td><span>Ext.</span></td>
-                        <td><span>UMF</span></td>
-                        <td><span>Tipo</span></td>
-                        <td><span>Fec-Mov</span></td>
-                        <td><span>C.Baja</span></td>
+                    <tr><td class="t_style3" colspan="10" style="text-align: center; width: 100%;"><span>Relación de movimientos operados</span></td></tr>
+                    <tr style="width: 100%;">
+                        <td class="t_style6" style="width:33px;"><span class="style3">Tipo</span></td>
+                        <td class="t_style6" style="width:85px;"><span class="style3">NSS</span></td>
+                        <td class="t_style6" style="width:235px;"><span class="style3">Nombre Asegurado</span></td>
+                        <td class="t_style6" style="width:61px;"><span class="style3">Sal.Base</span></td>
+                        <td class="t_style6" style="width:33px;"><span class="style3">Ext.</span></td>
+                        <td class="t_style6" style="width:30px;"><span class="style3">UMF</span></td>
+                        <td class="t_style6" style="width:36px;"><span class="style3">Tipo</span></td>
+                        <td class="t_style6" style="width:74px;"><span class="style3">Fec-Mov</span></td>
+                        <td class="t_style6" style="width:34px;"><span class="style3">Tipo</span></td>
+                        <td class="t_style6" style="width:65px;"><span class="style3">C.Baja</span></td>
                     </tr>
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td class="t_style5" style="width:33px;"><span class="style4">8</span></td>
+                        <td class="t_style5" style="width:85px;"><span class="style4">01048505299</span></td>
+                        <td class="t_style5" style="width:235px;"><span class="style4">BALTAZAR TORRES BEATRIZ MERCEDES</span></td>
+                        <td class="t_style5" style="width:61px;">
+                            <p class="style2" style="text-align: left;">$&nbsp;&nbsp;&nbsp;&nbsp;<span class="style4">145.10</span></p>
+                        </td>
+                        <td class="t_style5" style="width:33px;"><span class="style4">0</span></td>
+                        <td class="t_style5" style="width:30px;"><span class="style4">000</span></td>
+                        <td class="t_style5" style="width:36px;"><span class="style4">0</span></td>
+                        <td class="t_style5" style="width:74px;"><span class="style4">18/01/2021</span></td>
+                        <td class="t_style5" style="width:34px;"><span class="style4">1</span></td>
+                        <td class="t_style5" style="width:65px;"><span class="style4">0</span></td>
                     </tr>
-                </table>   
+                </table>
+                <p><span class="style2">Este documento es una representación impresa de los movimientos afiliatorios que fueron transmitidos, mismo que es extraído de la notaría.</span></p>
+                <footer>
+                    <p><span class="style2">Sello Digital:</span></p>
+                    <p><span class="style2">'.$variable21.'</span></p>
+                    <br />
+                    <img class="m_style2" src="img/piedp.png"/>
+                    <p style="text-align: right; margin: 0px 55px 0px 0px;">Página # de #</p>
+                </footer>
             </body>
             </html>');
         $html= mb_convert_encoding($html,'UTF-8','UTF-8');
